f13b60f11ea672ec0970227fd498c73ab8aaed51
Steven Schwengel <75495171+MustardTop@users.noreply.github.com>
2021-01-23 11:04:17 -0500

    progress bar and background color change fixed

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function formatAMPM(date) {
   var ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
   var strTime = hours + ':' + minutes + ' ' + ampm;
   return strTime;
 }
@@ -17,82 +17,101 @@ function formatAMPM(date) {
 
 function display_ct6() {
   var x = new Date()
-  var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-  hours = x.getHours( ) % 12;
+  var ampm = x.getHours() >= 12 ? ' PM' : ' AM';
+  hours = x.getHours() % 12;
   hours = hours ? hours : 12;
 
   lminutes = x.getMinutes();
   lseconds = x.getSeconds();
 
-/*  console.log(lhours, lminutes, lseconds); */
+  /*  console.log(lhours, lminutes, lseconds); */
 
-   if (hours < 10) {
-      hours = hours.toString().padStart(2, 0);
-    }
+  if (hours < 10) {
+    hours = hours.toString().padStart(2, 0);
+  }
 
   if (lminutes < 10) {
-      lminutes = lminutes.toString().padStart(2, 0);
-    }
+    lminutes = lminutes.toString().padStart(2, 0);
+  }
 
   if (lseconds < 10) {
-      lseconds = lseconds.toString().padStart(2, 0);
-      }
+    lseconds = lseconds.toString().padStart(2, 0);
+  }
 
-    var x1 = hours + ":" + lminutes + ":" + lseconds;
-//    var hexTime = hours + lminutes + lseconds;
+  var x1 = hours + ":" + lminutes + ":" + lseconds;
+  var hexTime = hours + lminutes + lseconds;
 
-    //console.log(hexTime);
-
-
-//  Percentage of Clock time part is below
+  //console.log(hexTime);
 
 
-   var boxProgress = lseconds / 60;
-   console.log(boxProgress.toFixed(2));
+  //  Percentage of Clock time part is below
+
+
+  var boxProgress = (lseconds / 60 * 14);
+  console.log("boxProgress", boxProgress.toFixed(2));
+
+  function move() {
+    var width = 1;
+    var bary = document.querySelector("#progress-bar");
+    bary.style.width = boxProgress + "%";
+    console.log("vary", bary);
+  }
+
+  move(boxProgress);
+
+
+
+  //
+  // function hexaTime() {
+  //   var date = new Date();
+  //   // we convert in the 0 .. 255 range
+  //   var hSeconds = parseInt(date.getSeconds() * 255 / 59);
+  //   var hMinutes = parseInt(date.getMinutes() * 255 / 59);
+  //   var hHours = parseInt(date.getHours() * 255 / 23);
+  //   return +hHours + hMinutes + hSeconds;
+  //   //
+  // }
+  // console.log("hexaTime",hexaTime());
 
 
 
 
-/*
-    function move(){
-        var width = 1;
-//        var elem = document.getElementById("clock-progress-bar");
-//        elem.style.width = boxProgress + "%"
-      }
+  function toHex(sec) {
+   var date = new Date();
+    var sec = parseInt(date.getSeconds() * 255 / 59);
+    var hex = ("0" + (Number(sec).toString(16))).slice(-3).toUpperCase();
+    var clock = document.querySelector(".clock");
+    clock.style.backgroundColor = "#" + hex;
+  //  clock.style.backgroundColor = "#CCC";
 
-     move(boxProgress);
+//    style.backgroundColor = "red";
 
-*/
+    console.log("returning hex",hex);
+  //    return hex;
+  }
+
+    toHex();
 
 
-function hexaTime() {
-  var date = new Date();
-  // we convert in the 0 .. 255 range
-  var hSeconds = parseInt(date.getSeconds() * 255 / 59);
-  var hMinutes = parseInt(date.getMinutes() * 255 / 59);
-  var hHours = parseInt(date.getHours() * 255 / 23);
-  return + hHours + hMinutes + hSeconds;
-//
-}
- console.log(hexaTime());
+  //  yourNumber = 100000;
+  //
+  // hexString = yourNumber.toString();
+  //
+  //    yourNumber=parseInt(hexString, 16);
+  //    console.log("yourNumber",yourNumber);
 
- // working model (after lecture is...
-//  progressBar.style.width = `${seconds / 60 * 14}rem`
-
-// MAIN Part of CODE
-
-// current hour : minute ; section function generator...
-
- document.getElementById('ct6').innerHTML = x1;
- //document.getElementById('ct7').innerHTML = hexTime;
+  document.querySelector(".clock-display").innerHTML = x1;
+  //document.getElementById('ct7').innerHTML = hexTime;
   display_c6();
 
 
- }
-function display_c6(){
-var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('display_ct6()',refresh)
 
+
+}
+
+function display_c6() {
+  var refresh = 1000; // Refresh rate in milli seconds
+  mytime = setTimeout('display_ct6()', refresh)
 }
 display_c6()
 
